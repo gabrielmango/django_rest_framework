713fddc646b4c54c8cc5c3a9eaba91b39f923384
Gabriel Mango <gabrielhmango@gmail.com>
2024-07-27 16:22:26 -0300

    creating serializers

---
+++ cursos/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+
+from .models import Curso, Avaliacao
+
+
+class AvaliacaoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Avaliacao
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+        fields = {
+            'id',
+            'nome',
+            'email',
+            'curso',
+            'avaliacao',
+            'comentario'
+            'criacao',
+            'atualizacao',
+            'ativo'
+        }
+
+
+class CursoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Curso
+        fields = {
+            'id',
+            'titulo',
+            'url',
+            'criacao',
+            'atualizacao',
+            'ativo'
+        }
