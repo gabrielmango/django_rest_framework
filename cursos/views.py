ea6942cefb60ec4a5e5d484e4be5e0ecedf9a315
Gabriel Mango <gabrielhmango@gmail.com>
2024-07-28 11:10:01 -0300

    creating pagination

--- cursos/views.py
+++ cursos/views.py
@@ -66,9 +66,16 @@ class CursoViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['get'])
     def avaliacoes(self, request, pk=None):
-        curso = self.get_object()
-        serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
-        return Response(serializer.data)
+        self.pagination_class.page_size = 1
+        avaliacoes = Avaliacao.objects.filter(curso_id=pk)
+        page = self.paginate_queryset(avaliacoes)
+
+        if page is not None:
+            serialiazer = AvaliacaoSerializer(page, many=True)
+            return self.get_paginated_response(serialiazer.data)
+        
+        serialiazer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serialiazer.data)
 
 
 class AvaliacaoViewSet(viewsets.ModelViewSet):
